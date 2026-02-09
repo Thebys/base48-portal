4aa5df2261ae923f242dc1b373ed6c9dc624403a
Tomáš "Thebys" Biheler <tomas@biheler.cz>
2026-02-09 15:55:50 +0100

    Bump version to 1.0.48 (RC1)

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
 in
 pkgs.buildGoModule rec {
   pname = "base48-portal";
-  version = "0.9.47";
+  version = "1.0.48";
   src = ./.;
 
   vendorHash = "sha256-IVv6aQMOIR8zil9AdMSekAfFVkFV/MD2mrPZoatGkqQ=";
