d32eaea7b40fa285aa6ef7740ec1d8a5dedee1f4
Tomáš "Thebys" Biheler <tomas@biheler.cz>
2026-03-11 01:48:20 +0100

    Bump version

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
 in
 pkgs.buildGoModule rec {
   pname = "base48-portal";
-  version = "1.2.50";
+  version = "1.3.37";
   src = ./.;
 
   vendorHash = "sha256-IVv6aQMOIR8zil9AdMSekAfFVkFV/MD2mrPZoatGkqQ=";
